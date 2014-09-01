9402fd1535a461f3f3d7b1bd1f8e413c358adc4d
Jonas Friedmann <j@frd.mn>
2014-09-01 18:10:35 +0200

    Substitute ViMbAdmin settings file

--- nginx/correct-vimbadmin-settings-file.sh
+++ nginx/correct-vimbadmin-settings-file.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 . /tmp/settings.conf
 
-sed 's/securitysalt/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/mailbox.uid/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/mailbox.gid/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/mailbox.homedir/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/options.driver/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/options.dbname/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/options.user/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/options.password/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
-#sed 's/options.host/'${VIMBSECSALT}'/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/securitysalt\s*=\s*\"\"$/securitysalt = \"'${VIMBSECSALT}'\"/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/defaults.mailbox.uid\s*=\s*2000$/defaults.mailbox.uid = 5000/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/defaults.mailbox.gid\s*=\s*2000$/defaults.mailbox.gid = 5000/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/defaults.mailbox.homedir\s*=\s*\"[a-zA-Z0-9_\/%]*\"$/defaults.mailbox.homedir = \"\/var\/vmail\/\"/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i "s/resources.doctrine2.connection.options.password\s*=\s*\'password\'$/resources.doctrine2.connection.options.password = \'"${MYSQLVIMBADMIN}"\'/g" /var/www/vimbadmin/application/configs/application.ini
