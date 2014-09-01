e834c49f8929b55f68ab04ecae9a1783bc9acffb
Jonas Friedmann <j@frd.mn>
2014-09-01 18:17:21 +0200

    Substitute new salts

--- nginx/correct-vimbadmin-settings-file.sh
+++ nginx/correct-vimbadmin-settings-file.sh
@@ -2,7 +2,9 @@
 . /tmp/settings.conf
 
 sed -i 's/securitysalt\s*=\s*\"\"$/securitysalt = \"'${VIMBSECSALT}'\"/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/resources.auth.oss.rememberme.salt\s*=\s*\"\"$/resources.auth.oss.rememberme.salt = \"'${VIMBREMEMBERSALT}'\"/g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's/defaults.mailbox.password_salt\s*=\s*\"\"$/defaults.mailbox.password_salt = \"'${VIMBPASSSALT}'\"/g' /var/www/vimbadmin/application/configs/application.ini
 sed -i 's/defaults.mailbox.uid\s*=\s*2000$/defaults.mailbox.uid = 5000/g' /var/www/vimbadmin/application/configs/application.ini
 sed -i 's/defaults.mailbox.gid\s*=\s*2000$/defaults.mailbox.gid = 5000/g' /var/www/vimbadmin/application/configs/application.ini
 sed -i 's/defaults.mailbox.homedir\s*=\s*\"[a-zA-Z0-9_\/%]*\"$/defaults.mailbox.homedir = \"\/var\/vmail\/\"/g' /var/www/vimbadmin/application/configs/application.ini
-sed -i "s/resources.doctrine2.connection.options.password\s*=\s*\'password\'$/resources.doctrine2.connection.options.password = \'"${MYSQLVIMBADMIN}"\'/g" /var/www/vimbadmin/application/configs/application.ini
+sed -i "s/resources.doctrine2.connection.options.password\s*=\s*'[a-zA-Z0-9]*'$/resources.doctrine2.connection.options.password = '"${MYSQLVIMBADMIN}"'/g" /var/www/vimbadmin/application/configs/application.ini
