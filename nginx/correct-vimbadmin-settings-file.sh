a8c16d06e953868b236d3903b617522d3c1aa476
Jonas Friedmann <j@frd.mn>
2014-09-03 17:32:16 +0200

    Change sed delimiter

--- nginx/correct-vimbadmin-settings-file.sh
+++ nginx/correct-vimbadmin-settings-file.sh
@@ -2,13 +2,13 @@
 . /tmp/settings.conf
 
 # TODO - improve sed to pass variables with special characters
-sed -i 's@^securitysalt\s*=\s*\"\"$@securitysalt = \"'${VIMBSECSALT}'\"@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i 's@^resources.auth.oss.rememberme.salt\s*=\s*\"\"$@resources.auth.oss.rememberme.salt = \"'${VIMBREMEMBERSALT}'\"@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i 's@^defaults.mailbox.password_salt\s*=\s*\"\"$@defaults.mailbox.password_salt = \"'${VIMBPASSSALT}'\"@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i 's@^defaults.mailbox.uid\s*=\s*2000$@defaults.mailbox.uid = 5000@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i 's@^defaults.mailbox.gid\s*=\s*2000$@defaults.mailbox.gid = 5000@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i 's@^defaults.mailbox.homedir\s*=\s*\"[a-zA-Z0-9_\/%]*\"$@defaults.mailbox.homedir = \"\/var\/vmail\/\"@g' /var/www/vimbadmin/application/configs/application.ini
-sed -i "s@^resources.doctrine2.connection.options.password\s*=\s*'[a-zA-Z0-9]*'@resources.doctrine2.connection.options.password = '${MYSQLVIMBADMIN}'@g" /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^securitysalt\s*=\s*\"\"$%securitysalt = \"'${VIMBSECSALT}'\"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^resources.auth.oss.rememberme.salt\s*=\s*\"\"$%resources.auth.oss.rememberme.salt = \"'${VIMBREMEMBERSALT}'\"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^defaults.mailbox.password_salt\s*=\s*\"\"$%defaults.mailbox.password_salt = \"'${VIMBPASSSALT}'\"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^defaults.mailbox.uid\s*=\s*2000$%defaults.mailbox.uid = 5000%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^defaults.mailbox.gid\s*=\s*2000$%defaults.mailbox.gid = 5000%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^defaults.mailbox.homedir\s*=\s*\"[a-zA-Z0-9_\/%]*\"$%defaults.mailbox.homedir = \"\/var\/vmail\/\"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i "s%^resources.doctrine2.connection.options.password\s*=\s*'[a-zA-Z0-9]*'$%resources.doctrine2.connection.options.password = '${MYSQLVIMBADMIN}'%g" /var/www/vimbadmin/application/configs/application.ini
 # TODO - check if not needed?
-#sed -i "s@^resources.doctrine2.connection.options.host\s*=\s*'localhost'$@resources.doctrine2.connection.options.host = '127.0.0.1'@g" /var/www/vimbadmin/application/configs/application.ini
-#sed -i 's@^resources.session.save_path@;resources.session.save_path@g' /var/www/vimbadmin/application/configs/application.ini
+#sed -i "s%^resources.doctrine2.connection.options.host\s*=\s*'localhost'$%resources.doctrine2.connection.options.host = '127.0.0.1'%g" /var/www/vimbadmin/application/configs/application.ini
+#sed -i 's%^resources.session.save_path$%;resources.session.save_path%g' /var/www/vimbadmin/application/configs/application.ini
