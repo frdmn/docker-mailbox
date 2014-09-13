5a6eb40230ebf4ef9a73f9f7f9d8a1d4d23380fe
Jonas Friedmann <j@frd.mn>
2014-09-14 01:53:38 +0200

    Adjust postfix's main.cf

--- postfix/adjust-postfix-configuration-file.sh
+++ postfix/adjust-postfix-configuration-file.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . /tmp/settings.conf
 
-sed -i -e "s/%primaryhost%/${MAILHOSTNAME}/g" /etc/postfix/main.cf
+sed -i -e "s/%mailhostname%/${MAILHOSTNAME}/g" /etc/postfix/main.cf
+sed -i -e "s/%primaryhostname%/${PRIMARYHOSTNAME}/g" /etc/postfix/main.cf
