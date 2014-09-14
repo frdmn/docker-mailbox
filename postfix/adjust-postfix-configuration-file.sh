03509cc378b40d19e9fb921de38b4b32d07114c5
Jonas Friedmann <j@frd.mn>
2014-09-14 03:05:37 +0200

    Substitute MySQL password for postfix configuration

--- postfix/adjust-postfix-configuration-file.sh
+++ postfix/adjust-postfix-configuration-file.sh
@@ -3,3 +3,7 @@
 
 sed -i -e "s/%mailhostname%/${MAILHOSTNAME}/g" /etc/postfix/main.cf
 sed -i -e "s/%primaryhostname%/${PRIMARYHOSTNAME}/g" /etc/postfix/main.cf
+
+sed -i -e "s/%mysqlvimbadmin%/${MYSQLVIMBADMIN}/g" /etc/postfix/mysql/virtual_alias_maps.cf
+sed -i -e "s/%mysqlvimbadmin%/${MYSQLVIMBADMIN}/g" /etc/postfix/mysql/virtual_domains_maps.cf
+sed -i -e "s/%mysqlvimbadmin%/${MYSQLVIMBADMIN}/g" /etc/postfix/mysql/virtual_mailbox_maps.cf
