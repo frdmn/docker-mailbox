c8f642731b7fa768f7cf70c04242953cd6182b47
Jonas Friedmann <j@frd.mn>
2014-10-27 00:14:49 +0100

    Use md5 unsalted (to test!)

--- nginx/correct-vimbadmin-settings-file.sh
+++ nginx/correct-vimbadmin-settings-file.sh
@@ -12,6 +12,7 @@ sed -i 's%^defaults.mailbox.maildir\s*=\s*\"maildir:/srv/vmail/\%d/\%u/mail:LAYO
 sed -i "s%^resources.doctrine2.connection.options.password\s*=\s*'[a-zA-Z0-9]*'$%resources.doctrine2.connection.options.password = '${MYSQLVIMBADMIN}'%g" /var/www/vimbadmin/application/configs/application.ini
 sed -i 's%^identity.email\s*=\s*"support@example.com"%identity.email = "support@'${PRIMARYHOSTNAME}'"%g' /var/www/vimbadmin/application/configs/application.ini
 sed -i 's%^server.email.address\s*=\s*"support@example.com"%server.email.address = "support@'${PRIMARYHOSTNAME}'"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^defaults.mailbox.password_scheme\s*=\s*"md5.salted"%defaults.mailbox.password_scheme = "md5"%g' /var/www/vimbadmin/application/configs/application.ini
 # TODO - check if not needed?
 #sed -i "s%^resources.doctrine2.connection.options.host\s*=\s*'localhost'$%resources.doctrine2.connection.options.host = '127.0.0.1'%g" /var/www/vimbadmin/application/configs/application.ini
 #sed -i 's%^resources.session.save_path$%;resources.session.save_path%g' /var/www/vimbadmin/application/configs/application.ini
