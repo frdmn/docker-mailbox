ec1be4360225da70870ae1a2691933ce168ae9c7
Jonas Friedmann <j@frd.mn>
2014-10-26 21:50:13 +0100

    Set proper MAIL FROM: for ViMbAdmin installation

--- nginx/correct-vimbadmin-settings-file.sh
+++ nginx/correct-vimbadmin-settings-file.sh
@@ -9,6 +9,8 @@ sed -i 's%^defaults.mailbox.uid\s*=\s*2000$%defaults.mailbox.uid = 5000%g' /var/
 sed -i 's%^defaults.mailbox.gid\s*=\s*2000$%defaults.mailbox.gid = 5000%g' /var/www/vimbadmin/application/configs/application.ini
 sed -i 's%^defaults.mailbox.homedir\s*=\s*\"[a-zA-Z0-9_\/%]*\"$%defaults.mailbox.homedir = \"\/var\/vmail\/\"%g' /var/www/vimbadmin/application/configs/application.ini
 sed -i "s%^resources.doctrine2.connection.options.password\s*=\s*'[a-zA-Z0-9]*'$%resources.doctrine2.connection.options.password = '${MYSQLVIMBADMIN}'%g" /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^identity.email\s*=\s*"support@example.com"%identity.email = "support@'${PRIMARYHOSTNAME}'"%g' /var/www/vimbadmin/application/configs/application.ini
+sed -i 's%^server.email.address\s*=\s*"support@example.com"%server.email.address = "support@'${PRIMARYHOSTNAME}'"%g' /var/www/vimbadmin/application/configs/application.ini
 # TODO - check if not needed?
 #sed -i "s%^resources.doctrine2.connection.options.host\s*=\s*'localhost'$%resources.doctrine2.connection.options.host = '127.0.0.1'%g" /var/www/vimbadmin/application/configs/application.ini
 #sed -i 's%^resources.session.save_path$%;resources.session.save_path%g' /var/www/vimbadmin/application/configs/application.ini
